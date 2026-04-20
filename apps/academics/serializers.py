6b2718113747e8031d09ead1a50324e8546bad33
yashyrg-gupta <yashraj.gupta@twinspark.co>
2026-04-20 12:56:50 +0530

    update research module

--- apps/academics/serializers.py
+++ apps/academics/serializers.py
@@ -1,145 +1,217 @@
 from rest_framework import serializers
 from .models import (
-    School, Department, Program, Course, CBCSCourse, DepartmentGallery, Notice, Committee, 
-    CommitteeMember, 
-    Timetable, StudyMaterial
+    School,
+    Department,
+    Program,
+    Course,
+    CBCSCourse,
+    DepartmentGallery,
+    Notice,
+    Committee,
+    CommitteeMember,
+    Timetable,
+    StudyMaterial,
 )
+
 
 class BaseSchoolSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+
     class Meta:
         model = School
-        fields = '__all__'
+        fields = "__all__"
+
     def get_image(self, obj):
         if obj.image:
             return obj.image.url
         return None
 
+
 class SchoolListSerializer(BaseSchoolSerializer):
     dean = serializers.SerializerMethodField()
+
     class Meta(BaseSchoolSerializer.Meta):
-        fields = ['id', 'name', 'slug', 'image', 'contact_email', 'contact_phone', 'dean']
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "image",
+            "contact_email",
+            "contact_phone",
+            "dean",
+        ]
 
     def get_dean(self, obj):
         from apps.faculty.serializers import FacultyListSerializer
-        dean = getattr(obj, 'dean', None)
+
+        dean = getattr(obj, "dean", None)
         if dean:
             return FacultyListSerializer(dean, context=self.context).data
         return None
 
+
 class SchoolDetailSerializer(BaseSchoolSerializer):
     dean = serializers.SerializerMethodField()
     departments = serializers.SerializerMethodField()
+
     class Meta:
         model = School
-        fields = '__all__'
+        fields = "__all__"
 
     def get_dean(self, obj):
         from apps.faculty.serializers import FacultyListSerializer
-        dean = getattr(obj, 'dean', None)
+
+        dean = getattr(obj, "dean", None)
         if dean:
             return FacultyListSerializer(dean, context=self.context).data
         return None
 
     def get_departments(self, obj):
-        return [{"id": dept.id, "name": dept.name, "slug": dept.slug} for dept in obj.departments.all()]
+        return [
+            {"id": dept.id, "name": dept.name, "slug": dept.slug}
+            for dept in obj.departments.all()
+        ]
+
 
 class DepartmentGallerySerializer(serializers.ModelSerializer):
     class Meta:
         model = DepartmentGallery
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BaseDepartmentSerializer(serializers.ModelSerializer):
-    school_name = serializers.CharField(source='school.name', read_only=True)
-    school_slug = serializers.CharField(source='school.slug', read_only=True)
+    school_name = serializers.CharField(source="school.name", read_only=True)
+    school_slug = serializers.CharField(source="school.slug", read_only=True)
+
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = "__all__"
+
 
 class DepartmentListSerializer(BaseDepartmentSerializer):
     hod = serializers.SerializerMethodField()
+
     class Meta(BaseDepartmentSerializer.Meta):
-        fields = ['id', 'name', 'slug', 'school', 'school_name', 'school_slug', 'contact_email', 'contact_phone', 'hod']
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "school",
+            "school_name",
+            "school_slug",
+            "contact_email",
+            "contact_phone",
+            "hod",
+        ]
 
     def get_hod(self, obj):
         from apps.faculty.serializers import FacultyListSerializer
-        hod = getattr(obj, 'hod', None)
+
+        hod = getattr(obj, "hod", None)
         if hod:
             return FacultyListSerializer(hod, context=self.context).data
         return None
+
 
 class DepartmentDetailSerializer(BaseDepartmentSerializer):
     hod = serializers.SerializerMethodField()
     gallery_images = DepartmentGallerySerializer(many=True, read_only=True)
+
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = "__all__"
 
     def get_hod(self, obj):
         from apps.faculty.serializers import FacultyListSerializer
-        hod = getattr(obj, 'hod', None)
+
+        hod = getattr(obj, "hod", None)
         if hod:
             return FacultyListSerializer(hod, context=self.context).data
         return None
 
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CBCSCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = CBCSCourse
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BaseProgramSerializer(serializers.ModelSerializer):
-    department_name = serializers.CharField(source='department.name', read_only=True)
-    school_name = serializers.CharField(source='department.school.name', read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
+    school_name = serializers.CharField(source="department.school.name", read_only=True)
+
     class Meta:
         model = Program
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ProgramListSerializer(BaseProgramSerializer):
     courses = CourseSerializer(many=True, read_only=True)
+
     class Meta(BaseProgramSerializer.Meta):
         fields = [
-            'id', 'name', 'level', 'duration', 'intake', 'department', 
-            'department_name', 'school_name', 'fees', 'courses', 'syllabus'
+            "id",
+            "name",
+            "level",
+            "duration",
+            "intake",
+            "department",
+            "department_name",
+            "school_name",
+            "fees",
+            "courses",
+            "syllabus",
         ]
+
 
 class ProgramDetailSerializer(BaseProgramSerializer):
     courses = CourseSerializer(many=True, read_only=True)
+
     class Meta(BaseProgramSerializer.Meta):
-        fields = '__all__'
+        fields = "__all__"
+
 
 class NoticeListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
-        exclude = ('content',)
+        fields = "__all__"
+
 
 class NoticeDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CommitteeMemberSerializer(serializers.ModelSerializer):
-    faculty_name = serializers.CharField(source='faculty.name', read_only=True)
+    faculty_name = serializers.CharField(source="faculty.name", read_only=True)
+
     class Meta:
         model = CommitteeMember
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CommitteeSerializer(serializers.ModelSerializer):
     members = CommitteeMemberSerializer(many=True, read_only=True)
+
     class Meta:
         model = Committee
-        fields = '__all__'
+        fields = "__all__"
+
 
 class TimetableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timetable
-        fields = '__all__'
+        fields = "__all__"
+
 
 class StudyMaterialSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyMaterial
-        fields = '__all__'
+        fields = "__all__"
--- apps/research/serializers.py
+++ apps/research/serializers.py
@@ -1,89 +1,131 @@
 from rest_framework import serializers
 from .models import (
-    ResearchArea, ResearchFacility, ResearchProject, ResearchScholar,
-    Publication, Patent, ResearchDevelopmentCellMember
+    ResearchArea,
+    ResearchFacility,
+    ResearchProject,
+    ResearchScholar,
+    Publication,
+    Patent,
+    ResearchDevelopmentCellMember,
 )
 from apps.faculty.serializers import FacultyListSerializer
 
+
 class ResearchAreaSerializer(serializers.ModelSerializer):
-    department_name = serializers.CharField(source='department.name', read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
+
     class Meta:
         model = ResearchArea
-        fields = ['id', 'name', 'description', 'department', 'department_name']
+        fields = [
+            "id",
+            "available_research_areas_or_Specialization",
+            "description",
+            "department",
+            "department_name",
+        ]
+
 
 class ResearchFacilitySerializer(serializers.ModelSerializer):
-    incharge_name = serializers.CharField(source='incharge.name', read_only=True)
+    incharge_name = serializers.CharField(source="incharge.name", read_only=True)
+
     class Meta:
         model = ResearchFacility
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ResearchProjectListSerializer(serializers.ModelSerializer):
-    pi_name = serializers.CharField(source='principal_investigator.name', read_only=True)
-    department_name = serializers.CharField(source='department.name', read_only=True)
-    department_slug = serializers.CharField(source='department.slug', read_only=True)
+    pi_name = serializers.CharField(
+        source="principal_investigator.name", read_only=True
+    )
+    department_name = serializers.CharField(source="department.name", read_only=True)
+    department_slug = serializers.CharField(source="department.slug", read_only=True)
     co_investigators_names = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = ResearchProject
         fields = [
-            'id', 'title', 'pi_name', 'co_investigators_names', 'funding_agency', 
-            'amount_sanctioned', 'status', 'department_name', 'department_slug'
+            "id",
+            "title",
+            "pi_name",
+            "co_investigators_names",
+            "funding_agency",
+            "amount_sanctioned",
+            "status",
+            "department_name",
+            "department_slug",
         ]
 
     def get_co_investigators_names(self, obj):
         return [faculty.name for faculty in obj.co_investigators.all()]
 
+
 class ResearchProjectDetailSerializer(serializers.ModelSerializer):
     principal_investigator = FacultyListSerializer(read_only=True)
     co_investigators = FacultyListSerializer(many=True, read_only=True)
-    department_name = serializers.CharField(source='department.name', read_only=True)
-    department_slug = serializers.CharField(source='department.slug', read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
+    department_slug = serializers.CharField(source="department.slug", read_only=True)
 
     class Meta:
         model = ResearchProject
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ResearchScholarListSerializer(serializers.ModelSerializer):
-    supervisor_name = serializers.CharField(source='supervisor.name', read_only=True)
-    co_supervisor_name = serializers.CharField(source='co_supervisor.name', read_only=True, default=None)
-    department_name = serializers.CharField(source='department.name', read_only=True)
-    department_slug = serializers.CharField(source='department.slug', read_only=True)
+    supervisor_name = serializers.CharField(source="supervisor.name", read_only=True)
+    co_supervisors_names = serializers.SerializerMethodField()
+    department_name = serializers.CharField(source="department.name", read_only=True)
+    department_slug = serializers.CharField(source="department.slug", read_only=True)
 
     class Meta:
         model = ResearchScholar
         fields = [
-            'id', 'scholar_name', 'enrollment_no', 'supervisor_name', 'co_supervisor_name',
-            'registration_year', 'status', 'department_name', 'department_slug'
+            "id",
+            "scholar_name",
+            "enrollment_no",
+            "subject",
+            "supervisor_name",
+            "co_supervisors_names",
+            "date_of_registration",
+            "status",
+            "department_name",
+            "department_slug",
         ]
+
+    def get_co_supervisors_names(self, obj):
+        return [faculty.name for faculty in obj.co_supervisor.all()]
+
 
 class ResearchScholarDetailSerializer(serializers.ModelSerializer):
     supervisor = FacultyListSerializer(read_only=True)
-    co_supervisor = FacultyListSerializer(read_only=True)
-    department_name = serializers.CharField(source='department.name', read_only=True)
+    co_supervisor = FacultyListSerializer(many=True, read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
 
     class Meta:
         model = ResearchScholar
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PublicationSerializer(serializers.ModelSerializer):
-    faculty_name = serializers.CharField(source='faculty.name', read_only=True)
-    department_name = serializers.CharField(source='department.name', read_only=True)
+    faculty_name = serializers.CharField(source="faculty.name", read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
 
     class Meta:
         model = Publication
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PatentSerializer(serializers.ModelSerializer):
-    faculty_name = serializers.CharField(source='faculty.name', read_only=True)
-    department_name = serializers.CharField(source='department.name', read_only=True)
+    faculty_name = serializers.CharField(source="faculty.name", read_only=True)
+    department_name = serializers.CharField(source="department.name", read_only=True)
 
     class Meta:
         model = Patent
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ResearchDevelopmentCellMemberSerializer(serializers.ModelSerializer):
     faculty = FacultyListSerializer(read_only=True)
 
     class Meta:
         model = ResearchDevelopmentCellMember
-        fields = ['id', 'faculty', 'designation', 'order']
+        fields = ["id", "faculty", "designation", "order"]
