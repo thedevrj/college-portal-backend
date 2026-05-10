d4d4106d912a66fd8e3959fece83172f9a60cfb3
yashyrg-gupta <yashraj.gupta@twinspark.co>
2026-05-10 15:51:15 +0530

    notice serialization

--- apps/notices/serializers.py
+++ apps/notices/serializers.py
@@ -3,13 +3,27 @@ from .models import GlobalNotice
 
 
 class GlobalNoticeListSerializer(serializers.ModelSerializer):
+    attachment = serializers.SerializerMethodField()
+
     class Meta:
         model = GlobalNotice
         # excluding heavy rich text payload for lists
         fields = "__all__"
 
+    def get_attachment(self, obj):
+        if obj.attachment:
+            return obj.attachment.url
+        return None
+
 
 class GlobalNoticeDetailSerializer(serializers.ModelSerializer):
+    attachment = serializers.SerializerMethodField()
+
     class Meta:
         model = GlobalNotice
         fields = "__all__"
+
+    def get_attachment(self, obj):
+        if obj.attachment:
+            return obj.attachment.url
+        return None
