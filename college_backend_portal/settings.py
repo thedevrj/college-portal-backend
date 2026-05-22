d19ba308977ed6f9657f30793fdb08b8d87b51c3
yashyrg-gupta <yashraj.gupta@twinspark.co>
2026-05-22 12:56:00 +0530

    add cross orign true

--- college_backend_portal/settings.py
+++ college_backend_portal/settings.py
@@ -33,6 +33,8 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
 
 # Application definition
 
