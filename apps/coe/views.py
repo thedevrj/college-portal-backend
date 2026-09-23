4098fe7ef5958bbc998a2da2644a743b84fff521
yashyrg-gupta <yashyrg01@gmail.com>
2026-09-23 16:45:36 +0530

    coe and accounts

--- apps/coe/views.py
+++ apps/coe/views.py
@@ -29,6 +29,19 @@ class COENoticeViewSet(viewsets.ModelViewSet):
             Q(is_archived=True) | Q(archive_date__lt=today)
         )
 
+class ArchiveCOENoticeViewSet(viewsets.ModelViewSet):
+    queryset = COENotice.objects.filter(is_archived=True).order_by("-date")
+    serializer_class = COENoticeSerializer
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        from django.utils import timezone
+        from django.db.models import Q
+
+        today = timezone.now().date()
+        return COENotice.objects.filter(
+            Q(is_archived=True) | Q(archive_date__lt=today)
+        )
 
 class PHDVivaVoceDateViewSet(viewsets.ModelViewSet):
     queryset = PHDVivaVoceDate.objects.all().order_by("-date")
@@ -44,6 +57,19 @@ class PHDVivaVoceDateViewSet(viewsets.ModelViewSet):
             Q(is_archived=True) | Q(archive_date__lt=today)
         )
 
+class ArchivePHDVivaVoceDateViewSet(viewsets.ModelViewSet):
+    queryset = PHDVivaVoceDate.objects.all().order_by("-date")
+    serializer_class = PHDVivaVoceDateSerializer
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        from django.utils import timezone
+        from django.db.models import Q
+
+        today = timezone.now().date()
+        return PHDVivaVoceDate.objects.filter(
+            Q(is_archived=True) | Q(archive_date__lt=today)
+        )
 
 class MPHILVivaVoceDateViewSet(viewsets.ModelViewSet):
     queryset = MPHILVivaVoceDate.objects.all().order_by("-date")
@@ -59,6 +85,19 @@ class MPHILVivaVoceDateViewSet(viewsets.ModelViewSet):
             Q(is_archived=True) | Q(archive_date__lt=today)
         )
 
+class ArchiveMPHILVivaVoceDateViewSet(viewsets.ModelViewSet):
+    queryset = MPHILVivaVoceDate.objects.all().order_by("-date")
+    serializer_class = MPHILVivaVoceDateSerializer
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        from django.utils import timezone
+        from django.db.models import Q
+
+        today = timezone.now().date()
+        return MPHILVivaVoceDate.objects.filter(
+            Q(is_archived=True) | Q(archive_date__lt=today)
+        )
 
 class PHDPreSubmissionSeminarViewSet(viewsets.ModelViewSet):
     queryset = PHDPreSubmissionSeminar.objects.all().order_by("-date")
@@ -74,6 +113,19 @@ class PHDPreSubmissionSeminarViewSet(viewsets.ModelViewSet):
             Q(is_archived=True) | Q(archive_date__lt=today)
         )
 
+class ArchivePHDPreSubmissionSeminarViewSet(viewsets.ModelViewSet):
+    queryset = PHDPreSubmissionSeminar.objects.all().order_by("-date")
+    serializer_class = PHDPreSubmissionSeminarSerializer
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        from django.utils import timezone
+        from django.db.models import Q
+
+        today = timezone.now().date()
+        return PHDPreSubmissionSeminar.objects.filter(
+            Q(is_archived=True) | Q(archive_date__lt=today)
+        )
 
 class RDCUNoticeViewSet(viewsets.ModelViewSet):
     queryset = RDCUNotice.objects.all().order_by("-date")
@@ -86,5 +138,20 @@ class RDCUNoticeViewSet(viewsets.ModelViewSet):
 
         today = timezone.now().date()
         return RDCUNotice.objects.exclude(
+            Q(is_archived=True) | Q(archive_date__lt=today)
+        )
+
+
+class ArchiveRDCUNoticeViewSet(viewsets.ModelViewSet):
+    queryset = RDCUNotice.objects.all().order_by("-date")
+    serializer_class = RDCUNoticeSerializer
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        from django.utils import timezone
+        from django.db.models import Q
+
+        today = timezone.now().date()
+        return RDCUNotice.objects.filter(
             Q(is_archived=True) | Q(archive_date__lt=today)
         )
